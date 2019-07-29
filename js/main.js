1d90ac90e7ea7d21f824fe30523d757db3160550
unknown <kipoph1@gmail.com>
2019-07-29 08:19:48 +0300

    Background color change working except for on new items

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 var listItems = document.getElementById("list").getElementsByTagName("li");
+var list = document.getElementById("list");
 var h1= document.getElementById("our-headline");
 var listLength=listItems.length;
+var button=document.getElementById("ourButton");
+var newItemCounter=1;
+document.write(listItems[1].innerText)
 for(i=0;i<listLength;i++)
 {
     listItems[i].addEventListener("click", activateItem);
 }
 function activateItem() {
+    for(i=0;i<listLength;i++)
+    {
+        listItems[i].classList.remove("active");
+    }
     h1.innerHTML=this.innerHTML;
+    this.classList.add("active");
+}
+
+button.addEventListener("click", createNewItem);
+
+function createNewItem() {
+    list.innerHTML += "<li>Somthing new number: "+newItemCounter+"<hr></li>";
+    newItemCounter++;
 }
