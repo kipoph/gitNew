0ae2b6a69caa20ca3c5a6a10e4fd67a093a0ae32
unknown <kipoph1@gmail.com>
2019-07-29 08:57:20 +0300

    cleaner code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var listItems = document.getElementById("list").getElementsByTagName("li");
 var list = document.getElementById("list");
 var h1= document.getElementById("our-headline");
-var listLength=listItems.length;
 var button=document.getElementById("ourButton");
 var newItemCounter=1;
 
 
 list.addEventListener("click", activateItem)
+button.addEventListener("click", createNewItem);
+
 function activateItem(e){
     if("LI"==e.target.nodeName){
         var ListLength = document.getElementById("list").getElementsByTagName("li").length;
@@ -19,8 +19,6 @@ function activateItem(e){
     }
     
 }
-document.write('hello there!')
-button.addEventListener("click", createNewItem);
 
 function createNewItem() {
     list.innerHTML += "<li>Somthing new number: "+newItemCounter+"<hr></li>";
